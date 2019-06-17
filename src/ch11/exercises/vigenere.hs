75fddd7a62cd28952839d6449f25aa30ce781f38
Karan Kumar <karan.kumar@habito.com>
2019-06-17 08:31:14 +0100

    finish vigenere

--- src/ch11/exercises/vigenere.hs
+++ src/ch11/exercises/vigenere.hs
@@ -2,30 +2,38 @@ module Vigenere where
 
 import Data.Char
 
--- simple Vigenère cipher
+-- simple Vigenère cipher for lowercase strings (converts a string to lowercase)
 
 type Key = String
 key :: String
 -- key = "haskell"
 key = "ally"
 
+aInt :: Int
+aInt = ord 'a' -- = 97
+
 zInt :: Int
-zInt = ord 'z'
+zInt = ord 'z' -- = 122
 
 vigenereEncode :: String -> Key -> String
 vigenereEncode [] _          = []
 vigenereEncode xs []         = vigenereEncode xs key
-vigenereEncode (x:xs) (k:ks) = (chr $ (ord x + ord k)) : vigenereEncode xs ks
+vigenereEncode (x:xs) (k:ks) = rightShift x k : vigenereEncode xs ks
 
 -- 1st argument: char from input String
 -- 2nd argument: char from key String
+-- *** DOESN'T WORK ***
 rightShift :: Char -> Char -> Char
-rightShift x k = chr $ ord x + (getKeyShift k)
+rightShift x k = chr $ if (ord x + shiftBy) > zInt then (aInt + ((ord x + shiftBy) - zInt) - 1)
+                       else ord x + shiftBy
+  where shiftBy = getKeyShift k
 
 -- tells you how much to shift by
+-- *** WORKS ***
 getKeyShift :: Char -> Int
 getKeyShift x = ord x - ord (head key)
 
-
+-- *** WORKS ***
 runVigenere :: String -> String
-runVigenere (x:xs) = x : vigenereEncode xs (tail key)
+runVigenere (x:xs) = (toLower x) : vigenereEncode (map toLower withoutSpaces) (tail key)
+  where withoutSpaces = concat $ words xs
