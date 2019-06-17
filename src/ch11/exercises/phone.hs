b41a632018dd25ea32411bf39da19715eb7dc1e0
Karan Kumar <karan.kumar@habito.com>
2019-06-17 08:52:59 +0100

    add phone stubs

--- src/ch11/exercises/phone.hs
+++ src/ch11/exercises/phone.hs
@@ -12,3 +12,42 @@ module Phone where
 --   | Star "*^"
 --   | Zero "0+ "
 --   | Hash "#.,"
+
+-- 1)
+data ThePhone = ThePhone
+
+convo :: [String]
+convo =
+  ["Wanna play 20 questions",
+   "Ya",
+   "U 1st haha",
+   "Lol ok. Have u tasted alcohol",
+   "Lol ya",
+   "Wow ur cool hava. Ur turn",
+   "Ok. Do u think I am pretty Lol",
+   "Lol ya",
+   "Just making sure rofl ur turn"]
+
+-- validButtons = "1234567890*#"
+type Digit = Char
+
+-- Valid presses: 1 and up
+type Presses = Int
+
+reverseTaps :: ThePhone -> Char -> [(Digit, Presses)]
+reverseTaps = undefined
+
+cellPhonesDead :: ThePhone -> String -> [(Digit, Presses)]
+cellPhonesDead = undefined
+
+fingerTaps :: [(Digit, Presses)] -> Presses
+fingerTaps = undefined
+
+mostPopularLetter :: String -> Char
+mostPopularLetter = undefined
+
+coolestLtr :: [String] -> Char
+coolestLtr = undefined
+
+coolestWord :: [String] -> Char
+coolestWord = undefined
