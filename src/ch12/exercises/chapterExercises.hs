9c1d90ed9399760d48c2851952983f589fcc987a
Karan Kumar <karan.kumar@habito.com>
2019-06-20 09:02:16 +0100

    Add nats exercise

--- src/ch12/exercises/chapterExercises.hs
+++ src/ch12/exercises/chapterExercises.hs
@@ -16,7 +16,6 @@ replaceThe xs = concat $ intersperse " " replaced
 -- 2) takes a string, splits it into words, and counts then number of occurrences of the word
 -- "the" followed by a word beginning with a vowel
 
--- *** BROKEN ***   
 countTheBeforeVowel :: String -> Integer  
 countTheBeforeVowel xs = countTheBeforeVowel' (words xs) 
 
@@ -24,11 +23,52 @@ countTheBeforeVowel xs = countTheBeforeVowel' (words xs)
 countTheBeforeVowel' :: [String] -> Integer
 countTheBeforeVowel' xs
   | length xs < 2 = 0 -- handles empty list and singleton list
-  | otherwise     = if (head xs) == "the" && (head $ head $ tail xs) `elem` "aeiou"
+  | otherwise     = if (head xs) == "the" && (head $ head $ tail xs) `elem` vowels
                       then 1 + countTheBeforeVowel' (tail $ tail xs)
                     else countTheBeforeVowel' (tail xs)
 
-countVowel :: String -> Integer
--- countVowel []     = 0
--- countVowel (x:xs) = if x `elem` "aeiou" then 1 + countVowel xs else countVowel xs
-countVowel xs = foldr (\x -> if x `elem` "aeiou" then (+1) else (+0)) 0 xs
+countVowels :: String -> Integer
+-- countVowels []     = 0
+-- countVowels (x:xs) = if x `elem` "aeiou" then 1 + countVowel xs else countVowel xs
+countVowels xs = foldr (\x -> if x `elem` "aeiou" then (+1) else (+0)) 0 xs
+
+-- *** validate the word ***
+-- if the number of vowels exceeds the number of constants, return Nothing in "in many human languages,
+-- vowels rarely exceed the number of consonants, so when they do, it may indicate that the input
+-- isn't a word"
+
+newtype Word' =
+  Word' String
+  deriving (Eq, Show)
+
+vowels = "aeiou"
+
+mkWord :: String -> Maybe Word'
+mkWord xs | vowels > consonants = Nothing
+          | otherwise           = Just (Word' xs)
+  where vowels     = countVowels xs
+        consonants = (fromIntegral $ length xs) - vowels
+
+-- natural numbers
+
+data Nat =
+    Zero
+  | Succ Nat
+  deriving (Eq, Show)
+
+natToInteger :: Nat -> Integer
+natToInteger Zero     = 0
+natToInteger (Succ n) = 1 + natToInteger n
+
+-- *** BROKEN *** 
+integerToNat :: Integer -> Maybe Nat
+integerToNat n | n < 0     = Nothing
+               | n == 0    = Just Zero
+               | otherwise = Just $ Succ (integerToNat' (n-1))
+               -- | otherwise = Just $ Succ (integerToNat (n-1))
+
+-- helper function for cases where n > 0 (first case is simply a base case since the function is
+-- recursive -- the call to integerToNat' from integerToNat will not pass 0 as an argument)
+integerToNat' :: Integer -> Nat
+integerToNat' 0 = Zero
+integerToNat' n = Succ (integerToNat' (n-1))
