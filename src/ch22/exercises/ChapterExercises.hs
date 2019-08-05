dbb21659188941d14893ec6a48c1cd890fce538d
Karan Kumar <karan.n.j.kumar@gmail.com>
2019-08-05 21:03:08 +0100

    start chapter 22 exercises

---
+++ src/ch22/exercises/ChapterExercises.hs
@@ -0,0 +1,35 @@
+module ChapterExercises where
+
+import Control.Applicative
+import Data.Maybe
+
+x = [1, 2, 3]
+y = [4, 5, 6]
+z = [7, 8, 9]
+
+-- lookup :: Eq a => a -> [(a, b)] -> Maybe b
+
+-- zip x and y using 3 as the lookup key
+xs :: Maybe Integer
+xs = lookup 3 $ zip x y
+
+-- zip y and z using 6 as the lookup key
+ys :: Maybe Integer
+ys = lookup 6 $ zip y z
+
+-- zip x and y using 4 as the lookup key (will return Nothing)
+zs :: Maybe Integer
+zs = lookup 4 $ zip x y
+
+-- zip x and z using a variable lookup key
+z' :: Integer -> Maybe Integer
+z' n = lookup n $ zip x z
+
+x1 :: Maybe (Integer, Integer)
+x1 = (,) <$> xs <*> ys
+
+x2 :: Maybe (Integer, Integer)
+x2 = (,) <$> ys <*> zs
+
+x3 :: Maybe (Integer, Integer)
+x3 = (,) <$> ys <*> zs
