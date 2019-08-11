f7dfb7f38d17763bd58ac04dcf8ddfb4e0f0a22a
Karan Kumar <karan.n.j.kumar@gmail.com>
2019-08-11 21:36:40 +0100

    add applicative Moi instance

--- src/ch23/exercises/WriteStateYourself.hs
+++ src/ch23/exercises/WriteStateYourself.hs
@@ -14,12 +14,16 @@ instance Functor (Moi s) where
                                in (f x, s')
 
 -- 2) write the Applicative instance for State
+-- solution from https://github.com/CarlosMChica/HaskellBook/blob/master/chapter23/WriteStateYourself.hs
 instance Applicative (Moi s) where
   pure :: a -> Moi s a
-  pure a = ???
+  pure a = Moi $ \s -> (a, s)
 
   (<*>) :: Moi s (a -> b)
         -> Moi s a
         -> Moi s b
   (Moi f) <*> (Moi g) =
-    ???
+    -- Moi $ \s -> (f g, s)
+    Moi $ \s -> let (f', s') = f s
+                    (x, s'') = g s'
+                in (f' x, s')
