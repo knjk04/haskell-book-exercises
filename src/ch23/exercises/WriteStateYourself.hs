5ee0459f23f2d2a6e5a9aaf31b1a38761112dd94
Karan Kumar <karan.n.j.kumar@gmail.com>
2019-08-11 10:53:56 +0100

    add functor instance for Moi

--- src/ch23/exercises/WriteStateYourself.hs
+++ src/ch23/exercises/WriteStateYourself.hs
@@ -7,21 +7,19 @@ newtype Moi s a =
 
 -- 1) implement the Functor instance for State
 -- State :: (s -> (a, s)) -> State s a
+-- solution from https://github.com/CarlosMChica/HaskellBook/blob/master/chapter23/WriteStateYourself.hs
 instance Functor (Moi s) where
   fmap :: (a -> b) -> Moi s a -> Moi s b
-  -- fmap f (Moi g) = Moi $ f g
-  -- fmap f (Moi g) = Moi (f g, g)
-  -- fmap f (Moi g) = Moi (f g, fmap f g)
-  -- fmap f (Moi g) = Moi $ fmap f g
-  -- fmap f (Moi g) = Moi g
-  -- fmap f (Moi g) = Moi (f g)
-  -- fmap f (Moi g) = Moi (f (fst g), snd g)
-  -- fmap f (Moi g) = Moi (fst (f g), snd (f g))
+  fmap f (Moi g) = Moi $ \s -> let (x, s') = g s
+                               in (f x, s')
 
-  -- fmap f (Moi g) = Moi (f g)
-  -- fmap f (Moi g) = Moi (g, f g)
-  -- fmap f (Moi g) = Moi g
+-- 2) write the Applicative instance for State
+instance Applicative (Moi s) where
+  pure :: a -> Moi s a
+  pure a = ???
 
-  -- fmap f (Moi g) = runMoi g 
-  -- fmap f (Moi g) = (snd (runMoi g))
-  fmap f (Moi g) = (snd g)
+  (<*>) :: Moi s (a -> b)
+        -> Moi s a
+        -> Moi s b
+  (Moi f) <*> (Moi g) =
+    ???
