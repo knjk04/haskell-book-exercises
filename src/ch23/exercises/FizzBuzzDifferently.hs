1dc297bbb21bc8ebf109c9112203b0f06eae97b3
Karan Kumar <karan.n.j.kumar@gmail.com>
2019-08-13 07:30:30 +0100

    start FizzBuzzDifferently exercise

---
+++ src/ch23/exercises/FizzBuzzDifferently.hs
@@ -0,0 +1,48 @@
+module FizzBuzzDifferently where
+
+import Control.Monad
+import Control.Monad.Trans.State
+
+-- fizzBuzz :: Integer -> String
+-- fizzBuzz n | n `mod` 15 == 0 = "FizzBuzz"
+--            | n `mod` 5  == 0 = "Buzz"
+--            | n `mod` 3  == 0 = "Fizz"
+--            | otherwise       = show n
+
+-- instead of changing the underlying data structure, fix the reversing FizzBuzz by
+-- changing the code to the following type signature
+-- still use cons when making the resulting list, but have the list come out in the
+-- right order to start with by enumerating the sequence in reverse
+fizzBuzzFromTo :: Integer
+               -> Integer
+               -> [String]
+fizzBuzzFromTo from to | from == to = [""]
+                       | from `mod` 15 == 0 = "FizzBuzz" : fizzBuzzFromTo (from + 1) to
+                       | from `mod` 5  == 0 = "Buzz"     : fizzBuzzFromTo (from + 1) to
+                       | from `mod` 3  == 0 = "Fizz"     : fizzBuzzFromTo (from + 1) to
+                       | otherwise          = show from  : fizzBuzzFromTo (from + 1) to
+
+-- mapM_ :: (Foldable t, Monad m) => (a -> m b) -> t a -> m ()
+-- execState :: State s a -> s -> s (in Control.Monad.Trans.State)
+-- fizzBuzzList :: [Integer] -> [String]
+-- fizzBuzzList list =
+--   execState (mapM_ addResult list) []
+
+fizzBuzzList :: [Integer] -> [String]
+fizzBuzzList list =
+  execState (mapM_ addResult list) []
+  
+-- get :: Monad m => StateT s m s 
+-- put :: Monad m => s -> StateT s m ()
+-- addResult :: Integer -> State [String] () -- old signature
+addResult :: Integer -> State [String] ()
+addResult n = do
+  xs <- get
+  let result = fizzBuzzFromTo n (n+1)
+  put (result : [xs])
+  -- put (result : concat xs)
+           
+-- main :: IO ()
+-- main =
+--   mapM_ putStrLn $
+--     reverse $ fizzBuzzList [1..100]
