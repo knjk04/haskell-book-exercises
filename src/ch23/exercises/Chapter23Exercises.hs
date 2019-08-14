42639fd1acbd9f125336fe9bea67be06098c1440
Karan Kumar <karan.kumar@habito.com>
2019-08-14 19:51:42 +0100

    add q4 and start q5

--- src/ch23/exercises/Chapter23Exercises.hs
+++ src/ch23/exercises/Chapter23Exercises.hs
@@ -41,6 +41,8 @@ instance Monad (State s) where
                   in runState (g x) s'
 
 -- 1) "Construct a State where the state is also the value you return"
+-- Expected output: Prelude> runState get "curryIsAmaze"
+-- ("curryIsAmaze","curryIsAmaze")
 get :: State s s
 get = State $ \s -> (s, s)
 
@@ -51,4 +53,15 @@ put s = State $ \_ -> ((), s)
 
 -- 3) "Run the State with s and get the state that results"
 exec :: State s a -> s -> s
-exec (State sa) s = snd $ runState (State sa) s
+-- exec (State sa) s = snd $ runState (State sa) s
+exec ssa@(State sa) s = snd $ runState ssa s
+
+-- 4) "Run the State with s and get the value that results"
+eval :: State s a -> s -> a
+-- eval (State sa) s = fst $ runState (State sa) s
+eval ssa@(State sa) s = fst $ runState ssa s
+
+-- 5) "Write a function which applies a function to create a new State"
+modify :: (s -> s) -> State s ()
+-- modify f = undefined
+modify f = put f
