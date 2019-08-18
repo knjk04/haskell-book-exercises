bdad5999f7dec8a24a6930f328b84ad4d9f6a938
Karan Kumar <karan.n.j.kumar@gmail.com>
2019-08-18 13:51:14 +0100

    add unit of success

--- src/ch24/exercises/UnitOfSuccess.hs
+++ src/ch24/exercises/UnitOfSuccess.hs
@@ -1,19 +1,17 @@
 module UnitOfSuccess where
 
 import Text.Trifecta
+import Data.Char
 
+-- Errors if you do parseString (integer >> eof) mempty "123abc"
+-- Prelude> parseString (integer >> eof) mempty "123"
+-- Success ()
+
+-- Need to rewrite the above example so that it returns the integer it parsed instead of Success ()
 -- Prelude> parseString unit mempty "123"
 -- Success 123
 
--- unit y@(x:xs) = integer >> eof 
-
--- unit :: String -> Parser String
--- unit = mapM char
-
--- unit y@(x:xs) = integer >> eof >> Success ()
-
--- unit :: String -> Parser Integer
--- unit = mapM integer
-
--- unit :: String -> Parser Integer
--- unit = integer 
+-- unit str = if all isDigit str then Success (read str :: Integer) else undefined
+-- solution below from https://github.com/CarlosMChica/HaskellBook/blob/master/chapter24/UnitOfSuccess.hs
+unit :: Parser Integer
+unit = integer <* eof
